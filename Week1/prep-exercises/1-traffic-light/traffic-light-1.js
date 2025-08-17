213400add681382bbb222783b063060b84685fa8
Majd Hamde <majdhamde1901@gmail.com>
2025-08-17 13:00:48 +0200

    solutin

--- Week1/prep-exercises/1-traffic-light/traffic-light-1.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-1.js
@@ -12,20 +12,13 @@ while (rotations < 2) {
   const currentState = trafficLight.state;
   console.log("The traffic light is on", currentState);
 
-  // TODO
-  // if the color is green, turn it orange
-  // if the color is orange, turn it red
-  // if the color is red, add 1 to rotations and turn it green
+  // logic to change the state
+  if (currentState === "green") {
+    trafficLight.state = "orange";
+  } else if (currentState === "orange") {
+    trafficLight.state = "red";
+  } else if (currentState === "red") {
+    rotations += 1;          // completed one full cycle
+    trafficLight.state = "green";
+  }
 }
-
-/**
- * The output should be:
-
-The traffic light is on green
-The traffic light is on orange
-The traffic light is on red
-The traffic light is on green
-The traffic light is on orange
-The traffic light is on red
-
-*/
