263968c18cb6ab37d58b408c312286f944d9e34b
majdjadalhaq <majdhamde1901@gmail.com>
2025-09-05 12:51:50 +0200

    Update ex3-object.js

--- Week4/prep-exercises/1-wallet/ex3-object.js
+++ Week4/prep-exercises/1-wallet/ex3-object.js
@@ -1,6 +1,10 @@
 import eurosFormatter from './euroFormatter.js';
 
 function createWallet(name, cash = 0) {
+  // Private(not that much ) properties using naming convention
+  let _dailyAllowance = 40;
+  let _dayTotalWithdrawals = 0;
+
   return {
     _name: name,
     _cash: cash,
@@ -15,15 +19,19 @@ function createWallet(name, cash = 0) {
         return 0;
       }
 
+      if (_dayTotalWithdrawals + amount > _dailyAllowance) {
+        console.log(`Insufficient remaining daily allowance!`);
+        return 0;
+      }
+// forgot what im doing mid way and just stare at the screen 
       this._cash -= amount;
+      _dayTotalWithdrawals += amount;
       return amount;
     },
 
     transferInto: function (wallet, amount) {
       console.log(
-        `Transferring ${eurosFormatter.format(amount)} from ${
-          this._name
-        } to ${wallet.getName()}`
+        `Transferring ${eurosFormatter.format(amount)} from ${this._name} to ${wallet.getName()}`
       );
       const withdrawnAmount = this.withdraw(amount);
       wallet.deposit(withdrawnAmount);
@@ -38,6 +46,17 @@ function createWallet(name, cash = 0) {
     getName: function () {
       return this._name;
     },
+
+    setDailyAllowance: function (newAllowance) {
+      _dailyAllowance = newAllowance;
+      console.log(
+        `Daily allowance set to: ${eurosFormatter.format(newAllowance)}`
+      );
+    },
+
+    resetDailyAllowance: function () {
+      _dayTotalWithdrawals = 0;
+    },
   };
 }
 
@@ -47,8 +66,10 @@ function main() {
   const walletJane = createWallet('Jane', 20);
 
   walletJack.transferInto(walletJoe, 50);
-  walletJane.transferInto(walletJoe, 25);
+  walletJack.setDailyAllowance(80);
+  walletJack.transferInto(walletJoe, 50);
 
+  walletJane.transferInto(walletJoe, 25);
   walletJane.deposit(20);
   walletJane.transferInto(walletJoe, 25);
 
